ef3c7bd645fb25ce89896b8d1a71e4ee02d505c4
Kyle Boon <kyle.f.boon@gmail.com>
2014-03-09 13:37:39 -0500

    Removing unnecessary stuff

--- site-cookbooks/kyleboon.me/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             "kyleboon.me"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures kyleboon.me"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
-
